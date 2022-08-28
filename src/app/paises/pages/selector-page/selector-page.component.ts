3dc9622cbb2f2c52e253e59530f72443cf3df59a
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-08-27 18:08:43 -0600

    2 selectores anidados. endpoint para obtener paises por continente

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PaisSmall } from '../../interfaces/paises.interface';
+import { PaisesService } from '../../services/paises-service.service';
+import { switchMap, tap } from 'rxjs/operators'
 
 @Component({
   selector: 'app-selector-page',
@@ -10,13 +13,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class SelectorPageComponent implements OnInit {
   
   miFormulario: FormGroup = this.fb.group({
-    region: ['', [Validators.required]]
+    region: ['', [Validators.required]],
+    pais: ['', [Validators.required]]
   })
+  //Llenar selectores
+  regiones: string[] = [];
+  paises: PaisSmall[] = [];
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private paisesService: PaisesService) { }
 
 
   ngOnInit(): void {
+    this.regiones = this.paisesService.regiones;
+    // Cuando cambie la region
+    /*this.miFormulario.get('region')?.valueChanges
+    .subscribe(region => {
+      this.paisesService.getPaisesPorRegion(region)
+      .subscribe( paises => { this.paises = paises; } )
+    })*/
+    this.miFormulario.get('region')?.valueChanges
+    .pipe(
+      tap(
+        ( _ ) => this.miFormulario.get('pais')?.reset('')
+      ),
+      switchMap( 
+        region => this.paisesService.getPaisesPorRegion(region) 
+      )
+    ).subscribe( (paises: PaisSmall[]) => { this.paises = paises; } )
+
+
   }
 
   guardar() {}
--- src/app/paises/services/paises-service.service.ts
+++ src/app/paises/services/paises-service.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PaisSmall } from '../interfaces/paises.interface';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PaisesServiceService {
+export class PaisesService {
 
-  constructor() { }
+  //JS -> los objetos pasan por referencia
+  private _regiones: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  private _baseUrl: string = 'https://restcountries.com/v2';
+
+  get regiones(): string[]{
+    return [...this._regiones];
+  }
+
+  constructor(private http: HttpClient) { }
+
+  getPaisesPorRegion(region: string) : Observable<PaisSmall[]> {
+    const url: string = `https://restcountries.com/v3.1/region/${region}?fields=name,cca2`;
+    return this.http.get<PaisSmall[]>(url)
+  }
 }
